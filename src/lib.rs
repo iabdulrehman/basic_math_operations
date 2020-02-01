d3c7c6a22318b2f4e397e162a0c568872497b21a
Abdul Rehman <rehman.abdul5666@gmail.com>
2020-02-02 01:23:53 +0500

    First Commit. Basic Functions Added

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+pub mod calculate
+{
+    pub fn addition(no1: u8, no2: u8)
+    {
+        println!("Sum : {}",no1 + no2);
+    }
+
+    pub fn subtraction(no1: u8, no2: u8)
+    {
+        println!("Sub : {}",no1 - no2);
+    }
+
+    pub fn multiplication(no1: u8, no2: u8)
+    {
+        println!("Mul : {}",no1 * no2);
+    }
+
+    pub fn division(no1: u8, no2: u8)
+    {
+        println!("Mul : {}",no1 / no2);
+    }
+}
